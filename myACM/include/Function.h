68eeb2f97defbcb2665b91dc957ee7d516422465
DESKTOP-065S5LG\Shinelon <james.masika@hotmail.com>
2017-03-27 23:51:40 +0800

    my acm project built in codeblocks

---
+++ myACM/include/Function.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+
+class Function
+{
+    public:
+        Function();
+        virtual ~Function();
+
+    protected:
+
+    private:
+};
+
+#endif // FUNCTION_H
